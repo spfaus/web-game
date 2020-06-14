aa4c0a0475a00368b655e8e837a47df8c2da8a1a
Simon-A-P <simon.pfaus@web.de>
2020-06-13 21:42:58 -0400

    Implement two-way databinding example

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LoginRegister text="Default text"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoginRegister from './components/LoginRegister'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginRegister
   }
 }
 </script>
---
+++ src/components/LoginRegister.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login-panel">
+        <h1 class="login-panel__title">{{ text }}</h1>
+        <input type="text" v-model="text">
+    </div>
+</template>
+
+    <script>
+export default {
+  name: 'LoginRegister',
+  props: {
+    text: String
+  }
+}
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
